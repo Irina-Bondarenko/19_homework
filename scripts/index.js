c0c7810600d94ed9f438385b7e9dee1ec508412b
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-05-04 21:29:38 +0300

    upd1

--- scripts/index.js
+++ scripts/index.js
@@ -12,56 +12,29 @@ function exponentiationFunction (number1, number2) {
     let number3 = number1 ** number2;
     alert(number3);
 }
-
 mainFunction(exponentiationFunction);
 
-
-/////////////
-
 // MULTIPLY
-
-function mainFunction2 (callback) {
-    let num1 = +prompt("Введите первое число");
-    let num2 = +prompt("Введите второе число");
-    callback (num1, num2);
-
-}
 
 function exponentiationMultiply (number1, number2) {
     let number3 = number1 * number2;
     alert(number3);
 }
 
-mainFunction2(exponentiationMultiply);
+mainFunction(exponentiationMultiply);
 
 // DIVISION
-
-function mainFunction3 (callback) {
-    let num1 = +prompt("Введите первое число");
-    let num2 = +prompt("Введите второе число");
-    callback (num1, num2);
-
-}
 
 function exponentiationDivision (number1, number2) {
     let number3 = number1 / number2;
     alert(number3);
 }
-
-mainFunction3(exponentiationDivision);
+mainFunction(exponentiationDivision);
 
 // MODULO
-
-function mainFunction4 (callback) {
-    let num1 = +prompt("Введите первое число");
-    let num2 = +prompt("Введите второе число");
-    callback (num1, num2);
-
-}
 
 function exponentiationModulo (number1, number2) {
     let number3 = number1 % number2;
     alert(number3);
 }
-
-mainFunction4(exponentiationModulo);
+mainFunction(exponentiationModulo);
